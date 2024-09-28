03f1ae0ebfa2837e9cb02e3433e318e97083784e
ksn199ms <mubashirpannur123@gmail.com>
2024-09-28 12:13:45 +0530

    Router setup completed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,22 @@
-import { Button } from "./components/ui/button"
+import { BrowserRouter, Routes , Route, Navigate } from "react-router-dom"
+
+import Auth from "./pages/Auth/Auth"
+import Chat from "./pages/Chat/Chat"
+import Profile from "./pages/Profile/Profile"
 
 
 function App() {
 
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='*' element={<Navigate to='/auth' />} />
+
+      <Route path='/auth' element={<Auth/>} />
+      <Route path='/chat' element={<Chat/>} />
+      <Route path='/profile' element={<Profile/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
