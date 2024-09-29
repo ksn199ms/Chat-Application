09e5a61388e478f5663bd52d48bd4cb5f1968737
ksn199ms <mubashirpannur123@gmail.com>
2024-09-29 12:00:44 +0530

    page router checking added

--- Client/src/pages/Chat/Chat.jsx
+++ Client/src/pages/Chat/Chat.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import { useAppStore } from '@/store'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
 
 const Chat = () => {
+
+  const {userInfo} = useAppStore()
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if(!userInfo.profileSetup){
+      toast.warning('Please setup your profile first')
+      navigate('/profile')
+    }}, [userInfo, navigate])
+
   return (
     <div>Chat</div>
   )
