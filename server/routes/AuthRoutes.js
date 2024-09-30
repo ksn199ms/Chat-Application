7ab4d69314d30cecb8f0f43b73d90744896cc3bf
ksn199ms <mubashirpannur123@gmail.com>
2024-09-30 13:07:03 +0530

    add profile image and delete profile image route configured in back-rnd

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,6 +1,10 @@
 import {Router} from 'express';
-import {getUserinfo, login, signup, updateProfile} from '../controllers/AuthController.js';
+import {getUserinfo, login, signup, updateProfile, addProfileImage, deleteProfileImage} from '../controllers/AuthController.js';
 import { verifyToken } from '../middlewares/AuthMiddleware.js';
+
+import multer from "multer";
+
+const upload = multer({dest:"uploads/profiles"});
 
 
 const authRoutes = Router();
@@ -9,5 +13,7 @@ authRoutes.post('/signup',signup);
 authRoutes.post('/login',login);
 authRoutes.get('/user-info',verifyToken,getUserinfo);
 authRoutes.post('/update-profile',verifyToken, updateProfile);
+authRoutes.post('/add-profile-image',verifyToken, upload.single('profile-image'), addProfileImage);
+authRoutes.delete('/delete-profile-image',verifyToken, deleteProfileImage);
 
 export default authRoutes;
