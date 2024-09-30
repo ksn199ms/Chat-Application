2f361138acc212322441fa470ec624c2c33beab5
ksn199ms <mubashirpannur123@gmail.com>
2024-09-30 22:15:57 +0530

    Chat Container Design completed

--- Client/src/pages/Chat/chat-container/ChatContainer.jsx
+++ Client/src/pages/Chat/chat-container/ChatContainer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import ChatHeader from './components/chat-header/ChatHeader'
+import MessageBar from './components/message-bar/MessageBar'
+import MessageContainer from './components/message-container/MessageContainer'
 
 const ChatContainer = () => {
   return (
-    <div>ChatContainer</div>
+    <div className='fixed top-0 h-[100vh] w-[100vw] bg-[#1c1d25] flex flex-col md:static md:flex-1 '>
+      <ChatHeader/>
+      <MessageContainer/>
+      <MessageBar/>
+    </div>
   )
 }
 
