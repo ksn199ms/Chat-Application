bc2ab7a792a2ee3559b36eebaa26fe2f08cfb8f4
ksn199ms <mubashirpannur123@gmail.com>
2024-09-30 22:14:37 +0530

    EmptyChatContainer commented for Designing Chat area

--- Client/src/pages/Chat/Chat.jsx
+++ Client/src/pages/Chat/Chat.jsx
@@ -23,7 +23,7 @@ const Chat = () => {
   return (
     <div className='flex h-[100vh] text-white overflow-hidden  '>
       <ContactsContainer/>
-      <EmptyChatContainer/>
+      {/* <EmptyChatContainer/> */}
       <ChatContainer/>
     </div>
   )
