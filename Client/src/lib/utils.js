f3c2b78c208a24c653f4b6f56a4e002e96d103b5
ksn199ms <mubashirpannur123@gmail.com>
2024-09-30 16:25:21 +0530

    Animation option added

--- Client/src/lib/utils.js
+++ Client/src/lib/utils.js
@@ -1,5 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
+import animationData from "@/assets/lottie-json"
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
@@ -18,3 +19,9 @@ export const getColor = (color) => {
   }
   return colors[0];
 };
+
+export const animationDefaultOption={
+  loop:true,
+  autoplay:true,
+  animationData:animationData,
+}
