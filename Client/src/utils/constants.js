51aa7e77d537068ba89c884153a0285256a568df
ksn199ms <mubashirpannur123@gmail.com>
2024-10-01 23:09:28 +0530

    search contact route added

--- Client/src/utils/constants.js
+++ Client/src/utils/constants.js
@@ -7,4 +7,9 @@ export const GET_USER_INFO = `${AUTH_ROUTES}/user-info`;
 export const UPDATE_PROFILE_ROUTE = `${AUTH_ROUTES}/update-profile`;
 export const ADD_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/add-profile-image`;
 export const DELETE_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}//delete-profile-image`;
-export const LOGOUT_ROUTE = `${AUTH_ROUTES}/logout`
+export const LOGOUT_ROUTE = `${AUTH_ROUTES}/logout`;
+
+
+export const CONTACT_ROUTES = "api/contacts";
+
+export const SEARCH_CONTACTS_ROUTE = `${CONTACT_ROUTES}/search`;
