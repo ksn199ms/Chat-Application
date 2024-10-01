1a83d5bd21f55d1785c401c267cf3f8142dc91d7
ksn199ms <mubashirpannur123@gmail.com>
2024-10-01 18:39:48 +0530

    Logout route setup completed

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import {getUserinfo, login, signup, updateProfile, addProfileImage, deleteProfileImage} from '../controllers/AuthController.js';
+import {getUserinfo, login, signup, updateProfile, addProfileImage, deleteProfileImage, logout} from '../controllers/AuthController.js';
 import { verifyToken } from '../middlewares/AuthMiddleware.js';
 
 import multer from "multer";
@@ -15,5 +15,6 @@ authRoutes.get('/user-info',verifyToken,getUserinfo);
 authRoutes.post('/update-profile',verifyToken, updateProfile);
 authRoutes.post('/add-profile-image',verifyToken, upload.single('profile-image'), addProfileImage);
 authRoutes.delete('/delete-profile-image',verifyToken, deleteProfileImage);
+authRoutes.post('/logout',logout);
 
 export default authRoutes;
