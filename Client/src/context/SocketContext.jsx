91d7be6e3734e4e6ab091e5113055a31bf27afed
ksn199ms <mubashirpannur123@gmail.com>
2024-10-02 10:35:22 +0530

    socket.io front-end configured

---
+++ Client/src/context/SocketContext.jsx
@@ -0,0 +1,34 @@
+import { useAppStore } from "@/store";
+import { HOST } from "@/utils/constants";
+import {createContext, useContext, useEffect, useRef} from "react";
+import { io } from "socket.io-client";
+
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+    return useContext(SocketContext);
+}
+
+export const SocketProvider = ({children}) => {
+    const socket = useRef();
+    const {userInfo} = useAppStore();
+    useEffect(() => {
+        if(userInfo) {
+            socket.current = io(HOST,{withCredentials: true,query:{userId: userInfo.id}});
+
+            socket.current.on('connect', () => {
+                console.log('connected to socket server');
+            });
+
+            return () => {
+                socket.current.disconnect();
+            }
+        }
+    }, [userInfo]);
+
+    return (
+        <SocketContext.Provider value={socket.current}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
