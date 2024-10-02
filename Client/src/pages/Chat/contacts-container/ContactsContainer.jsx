9a9616d27df8e011f0caaf7d236d22f9e1cd5462
ksn199ms <mubashirpannur123@gmail.com>
2024-10-02 20:04:48 +0530

    back-end just integreated

--- Client/src/pages/Chat/contacts-container/ContactsContainer.jsx
+++ Client/src/pages/Chat/contacts-container/ContactsContainer.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ProfileInfo from './components/profile-container/ProfileInfo';
 import NewDm from './components/new-dm/NewDm';
+import { apiClient } from '@/lib/api-client';
+import { GET_DM_CONTACTS_ROUTE } from '@/utils/constants';
 
 
 const ContactsContainer = () => {
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const response = await apiClient.get(GET_DM_CONTACTS_ROUTE, { withCredentials: true })
+
+      if(response.data.contacts) {
+        console.log(response.data.contacts);
+      }
+    }
+
+    getContacts()
+  }, [])
+
   return (
     <div className='relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full '>
         <div className="pt-3">
