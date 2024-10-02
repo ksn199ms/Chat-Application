b20ddc0475bbe37bf74c613cca8bf5969895b33e
ksn199ms <mubashirpannur123@gmail.com>
2024-10-02 16:43:23 +0530

    Send message functionality added

--- Client/src/pages/Chat/chat-container/components/message-bar/MessageBar.jsx
+++ Client/src/pages/Chat/chat-container/components/message-bar/MessageBar.jsx
@@ -1,3 +1,5 @@
+import { useSocket } from '@/context/SocketContext'
+import { useAppStore } from '@/store'
 import EmojiPicker from 'emoji-picker-react'
 import React, { useEffect, useRef, useState } from 'react'
 
@@ -6,6 +8,9 @@ import { IoSend } from 'react-icons/io5'
 import { RiEmojiStickerLine } from 'react-icons/ri'
 
 const MessageBar = () => {
+    const {selectedChatData, selectedChatType , userInfo} =useAppStore()
+
+    const socket = useSocket()
 
     const emojiRef = useRef();
 
@@ -30,7 +35,16 @@ const MessageBar = () => {
 
     const handleSendMessage = async () => {
 
-    }
+        if(selectedChatType === "contact"){
+            socket.emit("sendMessage", {
+                sender: userInfo.id,
+                content: message,
+                recipient: selectedChatData._id,
+                messageType: "text",
+                fileUrl: undefined,
+            })
+        }
+    };
 
     return (
         <div className='h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-6 gap-6'>
