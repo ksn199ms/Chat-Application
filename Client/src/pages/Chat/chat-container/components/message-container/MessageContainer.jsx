5a79b2b885c82cc9e9e1948c28db4fefe4228b45
ksn199ms <mubashirpannur123@gmail.com>
2024-10-02 16:44:38 +0530

    Chat area design and minimal functionality added

--- Client/src/pages/Chat/chat-container/components/message-container/MessageContainer.jsx
+++ Client/src/pages/Chat/chat-container/components/message-container/MessageContainer.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import { useAppStore } from "@/store";
+import moment from "moment";
+import React, { useEffect, useRef } from "react";
 
 const MessageContainer = () => {
-  return (
-    <div className='flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full '>
-        
-    </div>
-  )
-}
+  const scrollRef = useRef();
 
-export default MessageContainer
+  const { selectedChatType, selectedChatData, userInfo, selectedChatMessages } =
+    useAppStore();
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [selectedChatMessages]);
+
+  const renderMessages = () => {
+    let lastDate = null;
+    return selectedChatMessages.map((message, index) => {
+      const messageDate = moment(message.timestamp).format("YYYY-MM-DD");
+      const showDate = messageDate !== lastDate;
+      lastDate = messageDate;
+      return (
+        <div key={index}>
+          {showDate && (
+            <div className="text-center text-gray-500 my-2">
+              {moment(message.timestamp).format("LL")}
+            </div>
+          )}
+          {selectedChatType === "contact" && renderMessageContent(message)}
+        </div>
+      );
+    });
+  };
+
+  const renderMessageContent = (message) => (
+    <div
+      className={`${
+        message.sender === selectedChatData._id ? "text-left" : "text-right"
+      }`}
+    >
+      {message.messageType === "text" && (
+        <div
+          className={`${
+            message.sender !== selectedChatData._id
+              ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50"
+              : "bg-[#2a2b33]/5 text-white/80 border-[#ffffff]/20"
+          } border inline-block p-4 rounded my-1 max-w-[50%] break-words`}
+        >
+          {message.content}
+        </div>
+      )}
+      <div className="text-xs text-gray-600">
+        {moment(message.timestamp).format("LT")}
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full">
+      {renderMessages()}
+      <div ref={scrollRef} />
+    </div>
+  );
+};
+
+export default MessageContainer;
