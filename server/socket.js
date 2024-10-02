9bbea02cd6fff23bd5babac53e3345ea412c38a8
ksn199ms <mubashirpannur123@gmail.com>
2024-10-02 11:03:40 +0530

    Send/Receive Message back-end functionality added

--- server/socket.js
+++ server/socket.js
@@ -1,38 +1,58 @@
-import {Server as SocketIOServer} from 'socket.io'
+import { Server as SocketIOServer } from "socket.io";
+import Message from "./models/MessagesModel.js";
 
 const setupSocket = (server) => {
-    const io = new SocketIOServer(server, {
-       cors: {
-           origin: process.env.ORIGIN,
-           methods: ['GET', 'POST'],
-           credentials: true
-       } 
-    });
+  const io = new SocketIOServer(server, {
+    cors: {
+      origin: process.env.ORIGIN,
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
 
-    const userSocketMap = new Map();
+  const userSocketMap = new Map();
 
-    const disconnect = (socket) => {
-        console.log(`User disconnected: ${socket.id}`);
-        for (const [userId, socketId] of userSocketMap) {
-            if (socketId === socket.id) {
-                userSocketMap.delete(userId);
-                break;
-            }
-        }
+  const disconnect = (socket) => {
+    console.log(`User disconnected: ${socket.id}`);
+    for (const [userId, socketId] of userSocketMap) {
+      if (socketId === socket.id) {
+        userSocketMap.delete(userId);
+        break;
+      }
+    }
+  };
+
+  const sendMessage = async (message) => {
+    const senderSocketId = userSocketMap.get(message.sender);
+    const recipientSocketId = userSocketMap.get(message.recipient);
+
+    const createMessage = await Message.create(message);
+
+    const messageData = await Message.findById(createMessage._id)
+      .populate("sender", "id email firstName lastName image color")
+      .populate("recipient", "id email firstName lastName image color");
+
+      if(recipientSocketId){
+        io.to(recipientSocketId).emit("receiveMessage", messageData);
+      }
+      if(senderSocketId){
+        io.to(senderSocketId).emit("receiveMessage", messageData);
+      }
+
+  };
+
+  io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    if (userId) {
+      userSocketMap.set(userId, socket.id);
+      console.log(`User connected: ${userId} with socket id: ${socket.id}`);
+    } else {
+      console.log("User not found");
     }
 
-    io.on('connection', (socket) => {
-        const userId = socket.handshake.query.userId;
-        if(userId){
-            userSocketMap.set(userId, socket.id);
-            console.log(`User connected: ${userId} with socket id: ${socket.id}`);
-        }else{
-            console.log('User not found');
-        }
+    socket.on("sendMessage", sendMessage);
+    socket.on("disconnect", () => disconnect(socket));
+  });
+};
 
-        socket.on('disconnect', () => disconnect(socket));
-    })
-}
-
-
-export default setupSocket
+export default setupSocket;
