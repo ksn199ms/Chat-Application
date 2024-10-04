e26dc216ae00909af01a54d14ec87ccae2606589
ksn199ms <mubashirpannur123@gmail.com>
2024-10-04 21:08:57 +0530

    loading function's defined

--- Client/src/store/slices/chat-slice.js
+++ Client/src/store/slices/chat-slice.js
@@ -6,6 +6,16 @@ export const createChatslice = (set, get) => ({
     selectedChatMessages: [],
     directMessagesContacts: [],
 
+    isUploading: false,
+    isDownloading: false,
+    fileUploadProgress: 0,
+    fileDownloadProgress: 0,
+
+    setIsUploading: (isUploading) => set({ isUploading }),
+    setIsDownloading: (isDownloading) => set({ isDownloading }),
+    setFileUploadProgress: (fileUploadProgress) => set({ fileUploadProgress }),
+    setFileDownloadProgress: (fileDownloadProgress) => set({ fileDownloadProgress }),
+
     setSelectedChatType: (selectedChatType) => set({ selectedChatType }),
     setSelectedChatData: (selectedChatData) => set({ selectedChatData }),
     setSelectedChatMessages: (selectedChatMessages) => set({ selectedChatMessages }),
