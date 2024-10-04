7ae578912d1ad13c2cd19533a3e704db7594bdd5
ksn199ms <mubashirpannur123@gmail.com>
2024-10-04 17:04:51 +0530

    Upload file route setup completed

--- server/routes/MessageRoutes.js
+++ server/routes/MessageRoutes.js
@@ -1,10 +1,14 @@
 import {Router} from 'express';
 import {verifyToken} from '../middlewares/AuthMiddleware.js';
-import {getMessages} from '../controllers/MessageController.js'
+import {getMessages, uploadFile} from '../controllers/MessageController.js'
+import multer from "multer";
 
 const messageRoutes = Router();
 
+const upload =  multer({dest:"uploads/files"});
+
 messageRoutes.post('/get-messages',verifyToken, getMessages);
+messageRoutes.post('/upload-file',verifyToken, upload.single('file'), uploadFile);
 
 export default messageRoutes;
 
