f66fa457b6c177ff5f72931e55a734d15ae7a170
ksn199ms <mubashirpannur123@gmail.com>
2024-10-04 17:06:56 +0530

    upload file function setup done

--- server/controllers/MessageController.js
+++ server/controllers/MessageController.js
@@ -1,4 +1,5 @@
 import Message from "../models/MessagesModel.js";
+import {mkdirSync, renameSync} from "fs";
 
 
 export const getMessages = async (req, res) => {
@@ -22,3 +23,25 @@ export const getMessages = async (req, res) => {
     return res.status(500).send("Something went wrong");
   }
 };
+
+export const uploadFile = async (req, res) => {
+  try {
+    if(!req.file) {
+      return res.status(400).send("file not found");
+    }
+
+    const date = Date.now();
+    const fileDir = `uploads/files/${date}`;
+    const fileName = `${fileDir}/${req.file.originalname}`;
+
+    mkdirSync(fileDir, { recursive: true });
+    renameSync(req.file.path, fileName);
+
+    return res.status(200).json({ filePath: fileName });
+
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Something went wrong");
+  }
+}
