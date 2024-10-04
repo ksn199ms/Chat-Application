0885c762783d887e029ff53e6c8de39d6a0c0876
ksn199ms <mubashirpannur123@gmail.com>
2024-10-04 21:26:55 +0530

    Get all contacts route defined

--- server/routes/ContactRoutes.js
+++ server/routes/ContactRoutes.js
@@ -1,11 +1,12 @@
 import {Router} from 'express'
 import {verifyToken} from '../middlewares/AuthMiddleware.js'
-import {getContactsForDMList, searchContacts} from '../controllers/ContactController.js'
+import {getAllContacts, getContactsForDMList, searchContacts} from '../controllers/ContactController.js'
 
 const contactRouter = Router()
 
 
 contactRouter.post('/search',verifyToken, searchContacts)
 contactRouter.get('/get-contacts-for-dm',verifyToken, getContactsForDMList)
+contactRouter.get('/get-all-contacts',verifyToken, getAllContacts)
 
 export default contactRouter
