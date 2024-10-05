d9fc91abe94a6e17c5a04591c0a7985b5f54ae62
ksn199ms <mubashirpannur123@gmail.com>
2024-10-05 12:48:27 +0530

    get user channel constant route defined

--- Client/src/utils/constants.js
+++ Client/src/utils/constants.js
@@ -24,3 +24,4 @@ export const UPLOAD_FILE = `${MESSAGE_ROUTES}/upload-file`
 export const CHANNEL_ROUTES = "api/channel";
 
 export const CREATE_CHANNEL = `${CHANNEL_ROUTES}/create-channel`
+export const GET_USER_CHANNELS = `${CHANNEL_ROUTES}/get-user-channels`
