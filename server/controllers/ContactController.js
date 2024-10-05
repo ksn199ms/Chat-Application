d5da9f234405378d10dc0cae00ccd7736e9a741e
ksn199ms <mubashirpannur123@gmail.com>
2024-10-05 10:08:56 +0530

    value userId added

--- server/controllers/ContactController.js
+++ server/controllers/ContactController.js
@@ -127,7 +127,8 @@ export const getContactsForDMList = async (req, res) => {
       const users = await User.find({ _id: { $ne: req.userId } }, "firstName lastName _id email");
 
       const contacts = users.map((user) => ({
-        label: user.firstName  ? user.firstName + " " + user.lastName : user.email
+        label: user.firstName  ? user.firstName + " " + user.lastName : user.email,
+        value: user._id
       }));
 
       return res.status(200).json({ contacts });
