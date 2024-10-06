d0f5553483f8c8e990b844f9d5375f5867be4868
ksn199ms <mubashirpannur123@gmail.com>
2024-10-06 19:51:51 +0530

    Padding added

--- Client/src/pages/Chat/chat-container/components/chat-header/ChatHeader.jsx
+++ Client/src/pages/Chat/chat-container/components/chat-header/ChatHeader.jsx
@@ -10,7 +10,7 @@ const ChatHeader = () => {
   const { closeChat, selectedChatData, selectedChatType } = useAppStore();
 
   return (
-    <div className="h-[10vh] border-b-2 border-[#2f303b] flex items-center justify-between px-20  ">
+    <div className="h-[10vh] border-b-2 border-[#2f303b] flex items-center justify-between px-8  ">
       <div className="flex gap-5 items-center w-full justify-between ">
         <div className="flex gap-3 items-center justify-center">
           <div className="w-12 h-12 relative">
