4c3c01822ca5a3a6c233e278fa2de533c9e7c5ac
ksn199ms <mubashirpannur123@gmail.com>
2024-10-06 10:28:02 +0530

    Channel sending and receiving UI created and condition checked

--- Client/src/pages/Chat/chat-container/components/message-container/MessageContainer.jsx
+++ Client/src/pages/Chat/chat-container/components/message-container/MessageContainer.jsx
@@ -6,6 +6,8 @@ import React, { useEffect, useRef, useState } from "react";
 import {MdFolderZip} from "react-icons/md"
 import {IoMdArrowDown, IoMdArrowRoundDown } from "react-icons/io"
 import { IoCloseSharp } from "react-icons/io5";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { getColor } from "@/lib/utils";
 
 const MessageContainer = () => {
   const scrollRef = useRef();
@@ -81,6 +83,7 @@ const MessageContainer = () => {
             </div>
           )}
           {selectedChatType === "contact" && renderMessageContent(message)}
+          {selectedChatType === "channel" && renderChannelMessageContent(message)}
         </div>
       );
     });
@@ -138,6 +141,81 @@ const MessageContainer = () => {
       </div>
     </div>
   );
+
+  const renderChannelMessageContent = (message) => {
+    return (
+      <div className={`mt-5 ${message.sender._id !== userInfo.id ? "text-left" : "text-right"}`}>
+        {message.messageType === "text" && (
+        <div
+          className={`${
+            message.sender._id === userInfo.id
+              ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50"
+              : "bg-[#2a2b33]/5 text-white/80 border-[#ffffff]/20"
+          } border inline-block p-4 rounded my-1 max-w-[50%] break-words ml-9`}
+        >
+          {message.content}
+        </div>
+      )}
+      {
+        message.messageType === "file" && (
+          <div
+          className={`${
+            message.sender._id === userInfo.id
+              ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50"
+              : "bg-[#2a2b33]/5 text-white/80 border-[#ffffff]/20"
+          } border inline-block p-4 rounded my-1 max-w-[50%] break-words cursor-pointer`}
+        >
+          {checkIfImage(message.fileUrl) ? (
+            <div>
+              <img
+              src={`${HOST}/${message.fileUrl}`}
+              alt="file"
+              className="w-full h-auto object-contain"
+              onClick={()=>{setShowImage(true);setImageURL(message.fileUrl)}}
+            />
+            </div>
+          ) : (
+            <div className="flex items-center justify-center gap-4">
+              <span className="text-white/8 text-3xl bg-black/20 rounded-full p-3">
+              <MdFolderZip/>
+              </span>
+              <span>{message.fileUrl.split("/").pop()}</span>
+              <span className="bg-black/20 p-3 text-2xl rounded-full hover:bg-black/50 cursor-pointer transition-all duration-300" onClick={()=>{downloadFile(message.fileUrl)}}><IoMdArrowRoundDown/></span>
+            </div>
+          ) }
+        </div>
+        )
+      }
+      {
+        message.sender._id !== userInfo.id ? (<div className="flex items-center justify-start gap-3">
+          <Avatar className="w-8 h-8 rounded-full overflow-hidden">
+              {message.sender.image && (
+                <AvatarImage
+                  src={`${HOST}/${message.sender.image}`}
+                  alt={`${message.sender.email}`}
+                  className="object-cover w-full h-full rounded-full"
+                  style={{ objectFit: "cover", width: "100%", height: "100%" }} // Ensure the image fits properly
+                />
+              )}
+                <AvatarFallback
+                  className={`uppercase h-8 w-8 text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(
+                    message.sender.color
+                  )}`}
+                  style={{ fontSize: "1.5rem" }}
+                >
+
+                   {message.sender.firstName ? message.sender.firstName.charAt(0) : message.sender.email.charAt(0)}
+                </AvatarFallback>
+            </Avatar>
+            <span className="text-sm text-white/60">{`${message.sender.firstName} ${message.sender.lastName}`}</span>
+            <span className="text-xs text-white/60">{moment(message.timestamp).format("LT")}</span>
+        </div>) : (
+          <div className="text-xs text-white/60 mt-1">{moment(message.timestamp).format("LT")}</div>
+        )
+      }
+      </div>
+    ) 
+  }
 
   return (
     <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full">
