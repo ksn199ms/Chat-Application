f3e323c5bedf93d1b3199e849b875ea3ef2c5677
ksn199ms <mubashirpannur123@gmail.com>
2024-10-06 10:29:10 +0530

    Back-end socket snd channel message defined

--- server/socket.js
+++ server/socket.js
@@ -1,5 +1,6 @@
 import { Server as SocketIOServer } from "socket.io";
 import Message from "./models/MessagesModel.js";
+import Channel from "./models/ChannelModel.js";
 
 const setupSocket = (server) => {
   const io = new SocketIOServer(server, {
@@ -41,6 +42,44 @@ const setupSocket = (server) => {
 
   };
 
+  const sendChannelMeassaage = async (message) => {
+    const { channelId, sender, content, messageType, fileUrl } = message;
+
+    const createMessage = await Message.create({
+      sender,
+      recipient: null,
+      content,
+      messageType,
+      timestamp: Date.now(),
+      fileUrl,
+    });
+
+    const messageData = await Message.findById(createMessage._id).populate("sender", "id email firstName lastName image color").exec();
+
+    await Channel.findByIdAndUpdate(channelId, {
+      $push: { messages: messageData._id },
+    });
+    const channel = await Channel.findById(channelId).populate("members");
+
+    const finalData = {
+      ...messageData._doc,
+      channelId: channel._id,
+    }
+
+    if(channel && channel.members){
+      channel.members.forEach((member) => {
+        const memberSocketId = userSocketMap.get(member._id.toString());
+        if (memberSocketId) {
+          io.to(memberSocketId).emit("receive-channel-message", finalData);
+        }
+      })
+      const adminSocketId = userSocketMap.get(channel.admin._id.toString());
+      if (adminSocketId) {
+        io.to(adminSocketId).emit("receive-channel-message", finalData);
+      }
+    }
+  };
+
   io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     if (userId) {
@@ -51,6 +90,7 @@ const setupSocket = (server) => {
     }
 
     socket.on("sendMessage", sendMessage);
+    socket.on("send-channel-message", sendChannelMeassaage);
     socket.on("disconnect", () => disconnect(socket));
   });
 };
