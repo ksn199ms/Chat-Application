1ec3b82accef8dcf3f3a41a28536b71e123f1d83
ksn199ms <mubashirpannur123@gmail.com>
2024-10-06 19:51:15 +0530

    Responsive design completed on message bar now its compatable on small devices

--- Client/src/pages/Chat/chat-container/components/message-bar/MessageBar.jsx
+++ Client/src/pages/Chat/chat-container/components/message-bar/MessageBar.jsx
@@ -1,3 +1,145 @@
+// import { useSocket } from '@/context/SocketContext'
+// import { apiClient } from '@/lib/api-client'
+// import { useAppStore } from '@/store'
+// import { UPLOAD_FILE } from '@/utils/constants'
+// import EmojiPicker from 'emoji-picker-react'
+// import React, { useEffect, useRef, useState } from 'react'
+
+// import { GrAttachment } from 'react-icons/gr'
+// import { IoSend } from 'react-icons/io5'
+// import { RiEmojiStickerLine } from 'react-icons/ri'
+
+// const MessageBar = () => {
+//     const {selectedChatData, selectedChatType , userInfo ,setIsUploading , setFileUploadProgress} =useAppStore()
+
+//     const socket = useSocket()
+
+//     const emojiRef = useRef();
+
+//     const fileInputRef = useRef();
+
+//     const [message, setMessage] = useState("")
+//     const [emojiPickerOpen, setEmojiPickerOpen] = useState(false)
+
+//     useEffect(() => {
+//         function handleClickOutside(event) {
+//             if (emojiRef.current && !emojiRef.current.contains(event.target)) {
+//                 setEmojiPickerOpen(false)
+//             }
+//         }
+//         document.addEventListener("mousedown", handleClickOutside);
+//         return () => {
+//             document.removeEventListener("mousedown", handleClickOutside);
+//         }
+//     }),[emojiRef]
+
+//     const handleAddEmoji = (emoji) => {
+//         setMessage((message) => message + emoji.emoji)
+//     }
+
+//     const handleSendMessage = async () => {
+
+//         if(selectedChatType === "contact"){
+//             socket.emit("sendMessage", {
+//                 sender: userInfo.id,
+//                 content: message,
+//                 recipient: selectedChatData._id,
+//                 messageType: "text",
+//                 fileUrl: undefined,
+//             })
+//         } else if(selectedChatType === "channel"){
+//             socket.emit("send-channel-message", {
+//                 sender: userInfo.id,
+//                 content: message,
+//                 channelId: selectedChatData._id,
+//                 messageType: "text",
+//                 fileUrl: undefined,
+//             });
+//         }
+//         setMessage("")
+//     };
+
+//     const handleFileUpload = () => {
+//         if(fileInputRef.current){
+//             fileInputRef.current.click()
+//         }
+//     }
+
+//     const handleAttachmentChange = async (e) => {
+//         try {
+//             const file = e.target.files[0];
+//             if(file){
+//                 const formData = new FormData();
+//                 formData.append("file",file);
+//                 setIsUploading(true)
+//                 const response = await apiClient.post(UPLOAD_FILE,formData,{withCredentials : true,onUploadProgress : data => {setFileUploadProgress(Math.round((100 * data.loaded) / data.total))}})
+
+//                 if(response.status === 200 && response.data){
+//                     setIsUploading(false)
+//                     if(selectedChatType === "contact"){
+//                         socket.emit("sendMessage", {
+//                             sender: userInfo.id,
+//                             content: undefined,
+//                             recipient: selectedChatData._id,
+//                             messageType: "file",
+//                             fileUrl: response.data.filePath,
+//                         })
+//                     } else if(selectedChatType === "channel"){
+//                         socket.emit("send-channel-message", {
+//                             sender: userInfo.id,
+//                             content: undefined,
+//                             channelId: selectedChatData._id,
+//                             messageType: "file",
+//                             fileUrl: response.data.filePath,
+//                         });
+//                     }
+                    
+//                 }
+//             }
+//             console.log({file});
+//         } catch (error) {
+//             setIsUploading(false)
+//             console.log(error);
+//         }
+//     }
+
+//     return (
+//         <div className='h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-6 gap-6'>
+//             <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-5 pr-5 ">
+//                 <input type="text" className='flex-1 p-5 bg-transparent rounded-md focus:border-none focus:outline-none ' placeholder='Enter Message'
+//                     value={message}
+//                     onChange={(e) => setMessage(e.target.value)}
+//                 />
+//                 <button className='text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all ' onClick={handleFileUpload}>
+//                     <GrAttachment className='text-2xl' />
+//                 </button>
+//                 <input type="file" ref={fileInputRef} className="hidden" onChange={handleAttachmentChange} />
+//                 <div className="relative">
+//                     <button className='text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all ' onClick={() => setEmojiPickerOpen(true)}>
+//                         <RiEmojiStickerLine className='text-2xl' />
+//                     </button>
+//                     <div>
+//                         <div className='absolute bottom-16 right-0' ref={emojiRef}>
+//                             <EmojiPicker 
+//                             open={emojiPickerOpen} onEmojiClick={handleAddEmoji} autoFocusSearch={false}
+//                             theme='dark'/>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//             <button className='bg-[#8417ff] rounded-md flex items-center justify-center p-5  focus:border-none focus:outline-none hover:bg-[#741bda] focus:bg-[#741bda] focus:text-white duration-300 transition-all '
+//             onClick={handleSendMessage}>
+//                 <IoSend className='text-2xl' />
+//             </button>
+//         </div>
+//     )
+// }
+
+// export default MessageBar
+
+
+
+
 import { useSocket } from '@/context/SocketContext'
 import { apiClient } from '@/lib/api-client'
 import { useAppStore } from '@/store'
@@ -104,19 +246,19 @@ const MessageBar = () => {
     }
 
     return (
-        <div className='h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-6 gap-6'>
-            <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-5 pr-5 ">
-                <input type="text" className='flex-1 p-5 bg-transparent rounded-md focus:border-none focus:outline-none ' placeholder='Enter Message'
+        <div className='h-[10vh] bg-[#1c1d25] flex justify-center items-center px-4 sm:px-8 mb-6 gap-4 sm:gap-6'>
+            <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-3 sm:gap-5 pr-3 sm:pr-5">
+                <input type="text" className='flex-1 p-3 sm:p-5 bg-transparent rounded-md focus:border-none focus:outline-none ' placeholder='Enter Message'
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
                 />
                 <button className='text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all ' onClick={handleFileUpload}>
-                    <GrAttachment className='text-2xl' />
+                    <GrAttachment className='text-xl sm:text-2xl' />
                 </button>
                 <input type="file" ref={fileInputRef} className="hidden" onChange={handleAttachmentChange} />
                 <div className="relative">
                     <button className='text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all ' onClick={() => setEmojiPickerOpen(true)}>
-                        <RiEmojiStickerLine className='text-2xl' />
+                        <RiEmojiStickerLine className='text-xl sm:text-2xl' />
                     </button>
                     <div>
                         <div className='absolute bottom-16 right-0' ref={emojiRef}>
@@ -127,9 +269,9 @@ const MessageBar = () => {
                     </div>
                 </div>
             </div>
-            <button className='bg-[#8417ff] rounded-md flex items-center justify-center p-5  focus:border-none focus:outline-none hover:bg-[#741bda] focus:bg-[#741bda] focus:text-white duration-300 transition-all '
+            <button className='bg-[#8417ff] rounded-md flex items-center justify-center p-3 sm:p-5 focus:border-none focus:outline-none hover:bg-[#741bda] focus:bg-[#741bda] focus:text-white duration-300 transition-all '
             onClick={handleSendMessage}>
-                <IoSend className='text-2xl' />
+                <IoSend className='text-xl sm:text-2xl' />
             </button>
         </div>
     )
