b337cc761087d3dcac637dd78d2470a4c7f794cf
ksn199ms <mubashirpannur123@gmail.com>
2024-10-06 22:43:35 +0530

    margin bottom increased

--- Client/src/pages/Chat/chat-container/components/message-bar/MessageBar.jsx
+++ Client/src/pages/Chat/chat-container/components/message-bar/MessageBar.jsx
@@ -246,7 +246,7 @@ const MessageBar = () => {
     }
 
     return (
-        <div className='h-[10vh] bg-[#1c1d25] flex justify-center items-center px-4 sm:px-8 mb-6 gap-4 sm:gap-6 pb-4 sm:pb-6 md:pb-safe'>
+        <div className='h-[10vh] bg-[#1c1d25] flex justify-center items-center px-4 sm:px-8 mb-8 gap-4 sm:gap-6 pb-4 sm:pb-6 md:pb-safe'>
             <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-3 sm:gap-5 pr-3 sm:pr-5">
                 <input type="text" className='flex-1 p-3 sm:p-5 bg-transparent rounded-md focus:border-none focus:outline-none ' placeholder='Enter Message'
                     value={message}
